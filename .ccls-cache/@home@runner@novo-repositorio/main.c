a53238c9aed989aeccf002fbe983b8657bc074bb
rsdsilva <114616294+rsdsilvainf@users.noreply.github.com>
2022-10-01 12:10:44 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@novo-repositorio/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ int main(){
+   int n;
+   printf("Digite um numero:\n");
+    scanf("%d",&n);
+   if(n > 0)
+     printf("Positivo:\n");
+   if(n < 0)
+     printf("negativo:\n");
+ }
